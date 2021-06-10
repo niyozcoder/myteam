ef3f3e805054cf91541188d3492a3a39d52deec8
NiyozNeo <niyoz2005@gmail.com>
2021-06-10 17:19:23 +0500

    first commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 var burger = document.querySelector(".burger");
+var body = document.querySelector(".body");
 var nav = document.querySelector(".header__nav");
 
 burger.addEventListener("click", function () {
   burger.classList.toggle("burger--active");
   nav.classList.toggle("header__nav--active");
+  body.classList.toggle("body--active");
 })
