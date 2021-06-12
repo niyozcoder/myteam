8ced4441cbcdad2a94449b7297d9fb65cfdb9782
NiyozNeo <niyoz2005@gmail.com>
2021-06-12 12:23:41 +0500

    first commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,74 @@
 var burger = document.querySelector(".burger");
 var body = document.querySelector(".body");
 var nav = document.querySelector(".header__nav");
+var first1 = document.querySelector(".first-1");
+var second1 = document.querySelector(".second-1");
+var btn1 = document.querySelector(".btn-1");
+var list1 = document.querySelector(".list-1");
 
 burger.addEventListener("click", function () {
   burger.classList.toggle("burger--active");
   nav.classList.toggle("header__nav--active");
   body.classList.toggle("body--active");
+})
+
+btn1.addEventListener("click", function () {
+  btn1.classList.toggle("directors__list-btn--active");
+  first1.classList.toggle("first--active");
+  second1.classList.toggle("second--active");
+  list1.classList.toggle("directors__list-item--active");
+})
+var first2 = document.querySelector(".first-2");
+var second2 = document.querySelector(".second-2");
+var btn2 = document.querySelector(".btn-2");
+var list2 = document.querySelector(".list-2");
+btn2.addEventListener("click", function () {
+  btn2.classList.toggle("directors__list-btn--active");
+  first2.classList.toggle("first--active");
+  second2.classList.toggle("second--active");
+  list2.classList.toggle("directors__list-item--active");
+})
+
+var first3 = document.querySelector(".first-3");
+var second3 = document.querySelector(".second-3");
+var btn3 = document.querySelector(".btn-3");
+var list3 = document.querySelector(".list-3");
+btn3.addEventListener("click", function () {
+  btn3.classList.toggle("directors__list-btn--active");
+  first3.classList.toggle("first--active");
+  second3.classList.toggle("second--active");
+  list3.classList.toggle("directors__list-item--active");
+})
+
+var first4 = document.querySelector(".first-4");
+var second4 = document.querySelector(".second-4");
+var btn4 = document.querySelector(".btn-4");
+var list4 = document.querySelector(".list-4");
+btn4.addEventListener("click", function () {
+  btn4.classList.toggle("directors__list-btn--active");
+  first4.classList.toggle("first--active");
+  second4.classList.toggle("second--active");
+  list4.classList.toggle("directors__list-item--active");
+})
+
+var first5 = document.querySelector(".first-5");
+var second5 = document.querySelector(".second-5");
+var btn5 = document.querySelector(".btn-5");
+var list5 = document.querySelector(".list-5");
+btn5.addEventListener("click", function () {
+  btn5.classList.toggle("directors__list-btn--active");
+  first5.classList.toggle("first--active");
+  second5.classList.toggle("second--active");
+  list5.classList.toggle("directors__list-item--active");
+})
+
+var first6 = document.querySelector(".first-6");
+var second6 = document.querySelector(".second-6");
+var btn6 = document.querySelector(".btn-6");
+var list6 = document.querySelector(".list-6");
+btn6.addEventListener("click", function () {
+  btn6.classList.toggle("directors__list-btn--active");
+  first6.classList.toggle("first--active");
+  second6.classList.toggle("second--active");
+  list6.classList.toggle("directors__list-item--active");
 })
